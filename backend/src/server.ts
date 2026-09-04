0ef6bc5f8b66635be0ec30bba0601ce30972fe61
sami157 <sami.eee157@gmail.com>
2026-09-04 19:37:36 +0600

    basic prisma setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,9 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('This is the server for CPGCBL Fleet Management');
 });
 
-app.listen(4000);
+app.listen(5001, () => {
+  console.log('Fleet management server is running')
+});
